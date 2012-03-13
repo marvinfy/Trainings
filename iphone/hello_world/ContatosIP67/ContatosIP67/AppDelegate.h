a4e9ccd3414df21aa5d8e028106064c3f5a1f8df
Marcus <marcusviniciusns@gmail.com>
2012-03-12 22:59:51 -0300

    no message

---
+++ iphone/hello_world/ContatosIP67/ContatosIP67/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContatosIP67
+//
+//  Created by ios2534 on 12/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UINavigationController *navigationController;
+@end
---
+++ iphone/hello_world/ContatosIP67/ListagemContatosController.h
@@ -0,0 +1,15 @@
+//
+//  ListagemContatosController.h
+//  ContatosIP67
+//
+//  Created by ios2534 on 12/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListagemContatosController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *contatos;
+
+@end
