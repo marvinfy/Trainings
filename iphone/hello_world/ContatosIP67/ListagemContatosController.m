f08850cfad956164ebe2b53a75c4efb4dce97ac8
Marcus <marcusviniciusns@gmail.com>
2012-03-14 22:52:10 -0300

    no message

--- iphone/hello_world/ContatosIP67/ListagemContatosController.m
+++ iphone/hello_world/ContatosIP67/ListagemContatosController.m
@@ -178,21 +178,25 @@
 }
 */
 
-/*
+
 // Override to support rearranging the table view.
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
 {
-}
-*/
+    Contato *c = [contatos objectAtIndex:fromIndexPath.row];
+    [contatos removeObjectAtIndex: fromIndexPath.row];
+    [contatos insertObject: c atIndex:toIndexPath.row];
 
-/*
+}
+
+
+
 // Override to support conditional rearranging of the table view.
 - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the item to be re-orderable.
     return YES;
 }
-*/
+
 
 #pragma mark - Table view delegate
 
