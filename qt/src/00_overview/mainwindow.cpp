1d7becac405fde38420a3a096511a1c73b00c776
Marcus Vinícius Neves Silva <marcusviniciusns@gmail.com>
2012-10-27 17:09:25 -0200

    Some changes

--- qt/src/00_overview/mainwindow.cpp
+++ qt/src/00_overview/mainwindow.cpp
@@ -2,11 +2,16 @@
 #include "ui_mainwindow.h"
 #include <QUrl>
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    m_progressBar(NULL)
 {
     ui->setupUi(this);
+
+    ui->menuShow->addSeparator();
+    ui->menuShow->addAction(ui->mainToolBar->toggleViewAction());
 }
 
 MainWindow::~MainWindow()
@@ -16,6 +21,59 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_webButton_clicked()
 {
-    QUrl url(ui->webEdit->text());
+    QString text(ui->webEdit->text());
+    if (text.indexOf("://") == -1)
+    {
+        text.prepend("http://");
+    }
+
+    QUrl url(text);
     ui->webView->setUrl(url);
+}
+
+void MainWindow::on_webView_loadStarted()
+{
+    if (m_progressBar == NULL)
+    {
+        m_progressBar = new QProgressBar();
+        m_progressBar->setRange(0, 100);
+        ui->statusBar->addWidget(m_progressBar, 1);
+        //m_progressBar->setParent(ui->statusBar);
+    }
+    ui->statusBar->showMessage("");
+    m_progressBar->setValue(0);
+    m_progressBar->show();
+}
+
+void MainWindow::on_webView_loadProgress(int progress)
+{
+    m_progressBar->setValue(progress);
+}
+
+void MainWindow::on_webView_loadFinished(bool ok)
+{
+    m_progressBar->hide();
+    if (ok)
+    {
+        ui->statusBar->showMessage("Success");
+    }
+    else
+    {
+        ui->statusBar->showMessage("Failure");
+    }
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    close();
+}
+
+void MainWindow::on_actionShow_Hide_broser_toggled(bool show)
+{
+    ui->webFrame->setVisible(show);
+}
+
+void MainWindow::on_actionSobre_triggered()
+{
+
 }
--- qt/src/00_overview/mainwindow.h
+++ qt/src/00_overview/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QProgressBar>
 
 namespace Ui {
 class MainWindow;
@@ -18,8 +19,21 @@ public:
 private slots:
     void on_webButton_clicked();
 
+    void on_webView_loadStarted();
+
+    void on_webView_loadProgress(int progress);
+
+    void on_webView_loadFinished(bool arg1);
+
+    void on_actionExit_triggered();
+
+    void on_actionShow_Hide_broser_toggled(bool arg1);
+
+    void on_actionSobre_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QProgressBar* m_progressBar;
 };
 
 #endif // MAINWINDOW_H
